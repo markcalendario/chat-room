8bc3935acd7081ae2a335736427135ad9dcf02ec
Mark Kenneth Calendario <markcalendario@gmail.com>
2022-08-18 21:05:39 +0800

    improve the user interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
+// Socket connection
+// Must be connected to the server
 const socket = io.connect("http://localhost:7777");
 
 function App() {
@@ -11,16 +13,18 @@ function App() {
 
   useEffect(() => {
 
+    // Listener for "receiveMessage" emit event
     socket.on("receiveMessage", (data) => {
       appendMessages(prev => [...prev, data]);
     });
 
-
+    // Listener for "announceJoin" emit event
     socket.on("announceJoin", data => {
       setJoinName(data.name)
     })
 
     return () => {
+      // clean up
       socket.off("receiveMessage")
       socket.off("announceJoin")
     }
@@ -34,40 +38,55 @@ function App() {
   }
 
   useEffect(() => {
-    console.log("called");
+    // Effect if room has changed
+
+    // Emit a "joinRoom" event 
     socket.emit("joinRoom", { room: room, previousRoom: previousRoom })
   }, [previousRoom, room])
 
   function sendMessage() {
     const messageText = document.getElementById('message-text').value
 
-    if (messageText === '') {
-      return;
-    }
-    document.getElementById('message-text').value = ""
+    if (messageText === '') return;
+
+    // Emit a sendMessage event and pass room ID and message text JSON
     socket.emit("sendMessage", { room: room, message: messageText });
+
+    document.getElementById('message-text').value = ""
   }
 
   return (
-    <div style={{ fontFamily: 'Arial' }} className="App">
+    <div className="App">
       {
         joinName === null
           ? null
-          : <p style={{ color: 'green' }}>{joinName} joined the chat.</p>
+          : <p className="join-announce">{joinName} joined the chat.</p>
       }
-      <input id="custom-room-id" placeholder="Select or input custom room" type="text" />
-      <button onClick={joinRoom}>Join</button>
-      <br /><br />
-      {
-        messages.map((message, index) => (
-          <h3 key={index}>{message}</h3>
-        ))
-      }
-      <br /><br />
-      <textarea id="message-text" placeholder="text message" />
-      <button onClick={sendMessage}>Send</button>
+      <div className="form-box">
+        <input id="custom-room-id" placeholder="Room" type="text" />
+        <button onClick={joinRoom}>Join</button>
+        {
+          room !== "" ?
+            <h4>You have joined room <span style={{ color: "green" }}>{room}</span></h4>
+            : null
+        }
+        <div className="messages">
+          {
+            messages.map((message, index) => (
+              <p key={index}>{message}</p>
+            ))
+          }
+        </div>
+        <textarea id="message-text" placeholder="text message" />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+      <Footer />
     </div>
   );
+}
+
+function Footer() {
+  return <p className="footer">Made with love by <a target="_blank" rel="noreferrer" href="https://github.com/markcalendario">Mark Kenneth Calendario</a></p>
 }
 
 export default App;
